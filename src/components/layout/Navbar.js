0fe2976842d4c0c75a4f9dcb9c35a565f83f2b26
iamharshitjoshi <joshiharshit67396@gmail.com>
2024-02-27 00:14:50 +0530

    lau

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -3,6 +3,9 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
+    <>
+     
+
     <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
       <div className="w-full flex flex-wrap items-center justify-between p-4">
 
@@ -21,6 +24,7 @@ const Navbar = () => {
           <button
             type="button"
             className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            onClick={window.location.href = "/login"}
           >
             Login
           </button>
@@ -82,6 +86,7 @@ const Navbar = () => {
         </div>
       </div>
     </nav>
+    </>
   );
 };
 
